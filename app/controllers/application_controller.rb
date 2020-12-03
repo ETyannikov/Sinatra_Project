5a91c36ea386ed0be5b1f14f0f3ce7bbf13d5c38
Eddy Tyannikov <etyannikov@gmail.com>
2020-12-03 23:38:44 +0000

    Went back and made a User class to handle login auth. Will make table for it next

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ require './config/environment.rb'
 
 class ApplcationController < ActiveRecord::Base
   
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  helpers do
+    def current_user
+      @current_user ||= User.find_by(id: session[:user])
+    end
+
+    def logged_in?
+      current_user != nil
+    end
+  end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,5 @@
 class Doctor < ActiveRecord::Base 
+  belongs_to :user
   has_many :patient
   has_many :vaccine
 end
