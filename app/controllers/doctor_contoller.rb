790bffbf36669a56b15bb2dd717778b1fe1c5aec
Eddy Tyannikov <etyannikov@gmail.com>
2020-12-05 22:27:42 +0000

    Built doctor, index, added to User model to try and fix login

--- app/controllers/doctor_contoller.rb
+++ app/controllers/doctor_contoller.rb
@@ -1,3 +1,14 @@
 class DoctorController < ApplicationController
   
-end
+  get '/doctor/index' do
+    if logged_in?
+      @patients = []
+        Patient.all.each do |patient|
+          if patient.doctor == current_user then @patients << patient end
+        end #each do
+      erb :'doctor/index'
+    else 
+      redirect to '/login'
+    end #if
+  end #'/doctor/index'
+end #DoctorController
