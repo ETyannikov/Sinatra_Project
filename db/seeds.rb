bcb581ab39f8583726900c12937aada24dcacf00
Eddy Tyannikov <etyannikov@gmail.com>
2020-12-15 22:09:13 +0000

    Made the seeds for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-tori = User.create(:username => "PineapplePrincess736", :full_name => "Tori Miller", :password_digest => "password", :is_doctor => false)
-royan = User.create(:username => "JuneBug11", :full_name => "Royan Tyannikov", :password_digest => "password", :is_doctor => true)
+tori = Patient.create(:id => 1, :user_id => 1, :full_name => "Tori Miller", :age => 16)
+royan = User.create(:id => 1, :username => "JuneBug11", :password_digest => "password")
+plzer = Vaccine.create(:name => "plzer", :disease => "COVID19", :time => "12/20/2020", :patient_id => 1, :user_id => 1)
+
