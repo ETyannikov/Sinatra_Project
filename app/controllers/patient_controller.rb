376c3babd2c753f3ac94b125e3a1aca192173476
Eddy Tyannikov <etyannikov@gmail.com>
2020-12-15 21:54:12 +0000

    Built patient index, need to redo seeds for testing

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,7 +1,15 @@
 class PatientController < ApplicationController
   
-  get '/patient' do
-    current_user
-    erb :'patients/index'
-  end
+    get '/patients' do
+    #if logged_in?
+      current_user
+      @patients = []
+        Patient.all.each do |patient|
+          @patients << patient
+        end #each do
+      erb :'patients/index'
+    #else 
+      #redirect to '/login'
+    #end #if
+  end #'/doctor/index'
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -49,17 +49,7 @@ class UserController < ApplicationController
   
   
   
-    get '/user/index' do
-    if logged_in?
-      @patients = []
-        Patient.all.each do |patient|
-          if patient.doctor.id == session[:user_id] then @patients << patient end
-        end #each do
-      erb :'doctor/index'
-    else 
-      redirect to '/login'
-    end #if
-  end #'/doctor/index'
+
   
   
   
