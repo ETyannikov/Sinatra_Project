3307cb143712b6dbff93e82667fce6fbfcda69ae
Eddy Tyannikov <etyannikov@gmail.com>
2020-12-16 02:37:17 +0000

    Made patient controller

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -12,4 +12,86 @@ class PatientController < ApplicationController
       #redirect to '/login'
     #end #if
   end #'/doctor/index'
+  
+  get '/patients/new' do
+    if logged_in?
+      patients
+      erb :'patients/create'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/patients' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/patients/new"
+      else
+        @vaccine = current_user.vaccine.build(name: params[:name], disease: params[:disease], time: params[:time])
+        if @vaccine.save
+          redirect to "/patients/#{@vaccine.id}"
+        else
+          redirect to "/patients/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/patients/:id' do
+    if logged_in?
+      @vaccine = Patient.find_by_id(params[:id])
+      erb :'patients/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/patients/:id/edit' do
+    if logged_in?
+      patients
+      @vaccine = Patient.find_by_id(params[:id])
+      if @vaccine && @vaccine.user_id == current_user.id
+        erb :'patients/edit'
+      else
+        redirect to '/patients'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/patients/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/patients/#{params[:id]}/edit"
+      else
+        @vaccine = Patient.find_by_id(params[:id])
+        if @vaccine && @vaccine.user_id == current_user.id
+          if @vaccine.update(name: params[:name], disease: params[:disease], time: params[:time])
+            redirect to "/patients/#{@vaccine.id}"
+          else
+            redirect to "/patients/#{@vaccine.id}/edit"
+          end
+        else
+          redirect to '/patients'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  delete '/patients/:id/delete' do
+    if logged_in?
+      @vaccine = Patient.find_by_id(params[:id])
+      if @vaccine && @vaccine.user == current_user
+        @vaccine.delete
+      end
+      redirect to '/patients'
+    else
+      redirect to '/login'
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-tori = Patient.create(:id => 1, :user_id => 1, :full_name => "Tori Miller", :age => 16)
-royan = User.create(:id => 1, :username => "JuneBug11", :password => "password")
+tori = Patient.create( :user_id => 1, :full_name => "Tori Miller", :age => 16)
+royan = User.create( :username => "JuneBug11", :password => "password")
 plzer = Vaccine.create(:name => "plzer", :disease => "COVID19", :time => "12/20/2020", :patient_id => 1, :user_id => 1)
 
